c62f2d52bce200ece5dab12b7b6e72da05042961
Anastasia Higgins <higgins.ana@gmail.com>
2023-04-10 10:53:14 -0700

    finished

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // inferred typing
+            // string interpolation
+            // ternary operator
+            var answer = 4;
+            var response = (answer < 9) ? "is less than nine" : "is greater than or equal to nine";
+            
+            Console.WriteLine(response);
         }
     }
 }
